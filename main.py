c6f2da22e4419609847f0742b3549df271bd900b
zrobertson466920 <zrobertson@uchicago.edu>
2018-03-28 21:18:31 -0500

    Added In Program Editing

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
+
 import task
-import json
 import operator
-from matplotlib import pyplot as mp
+import json
 
 def main():
 
@@ -14,36 +14,67 @@ def main():
     for key, value in item.items():
         order[key] = value['priority']
 
-    while (input("Would you like to add a task? (y/n) ") == 'y'):
-
-        ctime = float(input("Input estimate of time to complete the task.\n"))
-        dtime = float(input("Input estimate of time available to work on task.\n"))
-        importance = float(input("On a scale of (0 - 100) how important is the task? (Hint: Interpret as percentile)\n"))
-        desc = input("Write a short description of the task below.\n")
-
-        item[desc] = {'ctime' : ctime, 'dtime' : dtime, 'importance' : importance, 'time' : ctime/dtime, 'priority' : task.priority(ctime,dtime,importance)}
-        order[desc] = item[desc]['priority']
-
-    sorted_item = sorted(order.items(), key = operator.itemgetter(1))
-
-    print("Saving Tasks")
-    with open("tasks.json", 'w') as f:
-        json.dump(item,f)
-
+    sorted_order = sorted(order.items(), key = operator.itemgetter(1), reverse = True)
+    task.print_sort(sorted_order)
     print("\n")
-    counter = 0
-    x = []
-    y = []
-    for ita in sorted_item[::-1]:
-        counter += 1
-        print(str(counter) + ": " + ita[0])
-        x.append(item[ita[0]]['time'])
-        y.append(item[ita[0]]['importance'])
 
-    mp.scatter(x,y,s = 10)
-    for i in range(counter):
-        mp.annotate(str(i+1), (x[i],y[i]))
+    command = 'a'
+    while (command != 'q'):
+        command = input("Enter a command: Add Task (a), Delete Task (d), Edit Task (e), View Taks (v), Save & Quit (q) \n")
 
-    mp.show()
+        if (command == 'a'):
+
+            desc = input("Write a short description of the task below.\n")
+            importance = float(input("On a scale of (0 - 100) how important is the task? (Hint: Interpret as percentile)\n"))
+            dtime = float(input("Input estimate of time available to work on task.\n"))
+            ctime = float(input("Input estimate of time to complete the task.\n"))
+            item[desc] = {'ctime' : ctime, 'dtime' : dtime, 'importance' : importance, 'time' : ctime/dtime, 'priority' : 0}
+            task.priority(item[desc])
+            order[desc] = item[desc]['priority']
+
+        elif (command == 'd'):
+
+            index = int(input("Input index of finished task. \n")) - 1
+            del item[sorted_order[index][0]]
+            del order[sorted_order[index][0]]
+            del sorted_order[index]
+
+        elif (command == 'e'):
+
+            index = int(input("Input index of task to edit. \n")) - 1
+            field = 'd'
+            while (field != 'q'):
+
+                field = input("Enter field you'd like to edit: description (d), importance (i), due-time (t), est-time (e), quit (q) \n")
+
+                if (field == 'd'):
+                    desc = input("Enter new description. \n")
+                    item[desc] = item[sorted_order[index][0]]
+                    order[desc] = item[desc]['priority']
+                    del item[sorted_order[index][0]]
+                    del order[sorted_order[index][0]]
+                    del sorted_order[index]
+                elif (field == 'i'):
+                    importance = input("Enter new importance. \n")
+                    item[sorted_order[index][0]]['importance'] = float(importance)
+                    order[sorted_order[index][0]] = task.priority(item[sorted_order[index][0]])
+                elif (field == 't'):
+                    dtime = input("Enter new due-time. \n")
+                    item[sorted_order[index][0]]['dtime'] = float(dtime)
+                    order[sorted_order[index][0]] = task.priority(item[sorted_order[index][0]])
+                elif (field == 'e'):
+                    dtime = input("Enter new est-time. \n")
+                    item[sorted_order[index][0]]['ctime'] = float(dtime)
+                    order[sorted_order[index][0]] = task.priority(item[sorted_order[index][0]])
+
+        elif (command == 'v'):
+            sorted_order = sorted(order.items(), key = operator.itemgetter(1), reverse = True)
+            task.print_sort(sorted_order)
+
+        elif (command == 'q'):
+            sorted_order = sorted(order.items(), key = operator.itemgetter(1), reverse = True)
+            task.print_csort(sorted_order,item)
+            with open("tasks.json", 'w') as f:
+                json.dump(item,f)
 
 main()
--- task.py
+++ task.py
@@ -1,4 +1,6 @@
 import numpy as np
+import operator
+from matplotlib import pyplot as mp
 
 class Task():
     """Models tasks on a to-do list with priority and details fields"""
@@ -11,8 +13,31 @@ class Task():
         self.time = 100 * ctime/dtime
 
         self.importance = imp
-        self.priority = np.sqrt((100) * (ctime/dtime)**(ctime/dtime)+(imp)**(imp))
+        self.priority = np.sqrt((ctime/dtime)*(ctime/dtime)+(imp)*(imp))
         self.desc = desc
 
-def priority(ctime,dtime,imp):
-    return np.sqrt((100 * ctime/dtime)**(ctime/dtime)+(imp)**(imp))
+def  print_sort(sorted_order):
+    counter = 0
+    for ita in sorted_order:
+        counter += 1
+        print(str(counter) + ": " + ita[0])
+
+def print_csort(sorted_order,item):
+    print("\n")
+    counter = 0
+    x = []
+    y = []
+    for ita in sorted_order:
+        counter += 1
+        print(str(counter) + ": " + ita[0])
+        x.append(100 * item[ita[0]]['time'])
+        y.append(item[ita[0]]['importance'])
+
+    mp.scatter(x,y,s = 10)
+    for i in range(counter):
+        mp.annotate(str(i+1), (x[i],y[i]))
+
+    mp.show()
+
+def priority(item):
+    return np.sqrt((100 * item['ctime']/item['dtime'])*(100 * item['ctime']/item['dtime'])+(item['importance'])*(item['importance']))
